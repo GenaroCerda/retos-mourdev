ba80831bbaeb59fce4e232177c7a1f031b57757c
GenaroCerda <dr_cocoon@DESKTOP-TAK2NJR.localdomain>
2025-08-14 19:19:50 -0600

    00-python

---
+++ ejercicio1.py
@@ -0,0 +1,31 @@
+# https://www.python.org/downloads/
+
+# Comentario de una sola linea.
+
+"""
+Comentario 
+de
+varias
+Lineas 
+"""
+
+#Variable tipo string
+nombre = "Genaro"
+print(type(nombre))
+# Variable tipo int
+edad = 33
+print(type(edad))
+# Variable tipo Bool
+casado = False
+print(type(casado))
+#Variable tipo Null
+hijos = None
+print(type(hijos))
+# Variable tipo float
+pi = 3.14
+print(type(pi))
+# Variable tipo complex
+comp = 2 + 3j
+print(type(comp))
+
+print("Hola, Python")
